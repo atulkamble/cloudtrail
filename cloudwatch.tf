73c071d6b1c87b0282453cb9935e994b75200cb6
Atul Kamble <atul_kamble@hotmail.com>
2026-06-17 21:55:16 +0530

    code

--- cloudwatch.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_cloudwatch_log_group" "cloudtrail" {
-  name              = "/aws/cloudtrail/${var.trail_name}"
-  retention_in_days = var.log_retention_days
-  tags              = local.common_tags
-}
--- locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-common_tags = {
-project = "aws-cloudtrail-project"
-owner = "cloudnautic"
-region = data.aws_region.current.id
-account_id = data.aws_caller_identity.current.account_id
-managed_by = "terraform"
-}
-}
